4f02bfd0f4d8330b6410d668f9b6d6e6b50d4bc8
Shobhit Kumar Sharma <sharmashobhit1000@gmail.com>
2025-03-09 14:44:58 +0530

    Adjacency matrix implementation added

--- Graphs/graphs.cpp
+++ Graphs/graphs.cpp
@@ -8,29 +8,55 @@ template <typename T>
 
 class Graph
 {
-    unordered_map<T, vector<T>> adj;
+    // unordered_map<T, vector<T>> adj;
+    vector<vector<int>> adj;
 
 public:
-    void addNode(T u, T v, bool directed)
+    Graph(int nodes)
     {
-        // 0 -> undirected
-        // 1 -> directed
+        adj.resize(nodes, vector<int>(nodes, 0));
+    }
+    // void addNode(T u, T v, bool directed)
+    // {
+    //     // 0 -> undirected
+    //     // 1 -> directed
 
-        adj[u].push_back(v);
+    //     adj[u].push_back(v);
+    //     if (!directed)
+    //     {
+    //         adj[v].push_back(u);
+    //     }
+    // }
+
+    void addNode(int u, int v, bool directed)
+    {
+        adj[u][v] = 1;
         if (!directed)
         {
-            adj[v].push_back(u);
+            adj[v][u] = 1;
         }
     }
 
+    // void printGraph()
+    // {
+    //     for (auto i : adj)
+    //     {
+    //         cout << i.first << "-> ";
+    //         for (int j : i.second)
+    //         {
+    //             cout << j << ", ";
+    //         }
+    //         cout << endl;
+    //     }
+    // }
+
     void printGraph()
     {
-        for (auto i : adj)
+        for (int i = 0; i < adj.size(); i++)
         {
-            cout << i.first << "-> ";
-            for (int j : i.second)
+            for (int j = 0; j < adj[i].size(); j++)
             {
-                cout << j << ", ";
+                cout << adj[i][j] << " ";
             }
             cout << endl;
         }
@@ -39,7 +65,6 @@ public:
 
 int main()
 {
-    Graph<int> g;
 
     int nodes;
     cout << "Enter the number of nodes : ";
@@ -48,6 +73,8 @@ int main()
     int edges;
     cout << "Enter the number of edges : ";
     cin >> edges;
+
+    Graph<int> g(nodes);
 
     for (int i = 1; i <= edges; i++)
     {
